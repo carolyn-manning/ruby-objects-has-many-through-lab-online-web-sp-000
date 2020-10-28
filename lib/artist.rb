9bb2604a6ce974ad70a4d10587aa6bc662a55f74
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-28 03:25:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,13 +3,13 @@ class Artist
   @@all = []
 
   def initialize(name)
-    @name = name 
-    @@all << self 
+    @name = name
+    @@all << self
   end 
 
   def self.all
     @@all
-  end 
+  end
 
 
 end
